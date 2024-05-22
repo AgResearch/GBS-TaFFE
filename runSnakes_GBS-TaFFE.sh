c11c0a992665c8f7b2fb11bb7dfd47c5bfce7498
Ben Perry <ben.perry@agresearch.co.nz>
2024-05-23 10:57:00 +1200

    updating bash wrappers

--- runSnakes_GBS-TaFFE.sh
+++ runSnakes_GBS-TaFFE.sh
@@ -16,12 +16,8 @@ for i in $(ls -r results | grep SQ);
 do
 	echo $i;
 	
-	snakemake --profile config/slurm_profiles/eRI --config LIBRARY=$i --snakefile workflow/GBS-TaFFE.smk results/$i/00_QC/seqkit.report.KDR.txt &&
+	snakemake --profile config/slurm_profiles/eRI --config LIBRARY=$i --snakefile workflow/GBS-TaFFE.smk
 	
-	snakemake --profile config/slurm_profiles/eRI --config LIBRARY=$i --snakefile workflow/Ref-Free.smk &&
-	
-	ln -s /scratch/2023-mbie-rumen-gbs/GBS-TaFFE/results/$i /projects/2024-gbs_microbiomes_processing/Pipeline/ 
-
 done
 
 
